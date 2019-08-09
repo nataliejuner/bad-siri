f555b7bb65c5772dc9b703c7821fa14054c9ef5a
Natalie Reinicke <natalie@mjdinteractive.com>
2019-08-09 10:50:04 -0700

    pepehands first commit

---
+++ src/components/input.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Input extends React.Component {
+	render() {
+		return (
+			<>
+				<h2>Hello, {this.props.name}. {this.props.request}</h2>
+				<input type="text" defaultValue="" placeholder="Wutcha want? Butts?" onChange={this.props.siri} />
+			</>
+		)
+	}
+}
+
+export default Input;
