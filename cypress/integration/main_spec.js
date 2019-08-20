21f142a73bbb73755d16ed4328ee4dfe17cd1b07
Natalie Reinicke <natalie@mjdinteractive.com>
2019-08-20 15:17:00 -0700

    tried to get google search to work with redo of input

--- cypress/integration/main_spec.js
+++ cypress/integration/main_spec.js
@@ -6,4 +6,10 @@ describe('Goes to site', () => {
 		cy.get('[id="question"]').should('be.visible')
 		cy.get('[id="question"]').type('closest pizza place?')
 	})
+	it('checks that not asking in question form does nothing', () => {
+		cy.get('[id="question"]').should('be.empty')
+		cy.get('[id="question"]').type('beer')
+		cy.scrollTo('bottom')
+		cy.get('span[class="gcsc-find-more-on-google-query"]').should('include', 'beer')
+	})
 })
